8eb50aca697f0548b88f3eee81eb039d275a68ad
Jaden-Crotty <145504748+Jaden-Crotty@users.noreply.github.com>
2023-09-30 19:32:56 -0400

    Add files via upload
    first step

---
+++ Chpt2Ex.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Chapter 2 Code 
+
+# ## Algorithem workbench - number 1 
+# 
+# We need to write a code that always the user to enter his or her height. 
+
+# In[ ]:
+
+
+# Prompt the user to enter their height
+height = input("Please enter your height ")
+
+# Print a confirmation message
+print(f"Your height is {height} ")
+
+
+# In[ ]:
+
+
+
+
